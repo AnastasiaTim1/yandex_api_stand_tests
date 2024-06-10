9af6422a86804acf4bdc6997745fdc779130cd12
vyacheslav babich <Xopyc2109@gmail.com>
2024-06-10 23:01:08 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+
+URL_SERVICE = "https://33a7f6d2-86ae-4812-92d3-1dbeab0c9140.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
